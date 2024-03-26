fa578ad489f8eb7e74e09445a38d070dd3f1faf4
MagicBoonto <magic.boonto@gmail.com>
2024-03-26 21:46:56 +0000

    About complex lists

---
+++ advlist/animal-list.py
@@ -0,0 +1,8 @@
+# Define a list of animals with three-letter names
+animals = ["Fox", "Fly", "Ant", "Bee", "Cod", "Cat", "Dog", "Yak", "Cow", "Hen", "Koi", "Hog", "Jay", "Kit"]
+
+# Print the list of animals
+print("List of animals with three-letter names:")
+for animal in animals:
+    print(animal)
+
